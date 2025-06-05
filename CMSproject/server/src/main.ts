28157af396b5fcf8ac705319575635f2af40cebd
Longle290204 <longvis2004@gmail.com>
2025-06-05 09:59:28 +0700

    first commit

---
+++ CMSproject/server/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all routes
+  const port = 3003;
+  await app.listen(port);
+}
+
+bootstrap();
