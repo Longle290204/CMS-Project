13cd583fbbbbef9c491d7f96312f5572ba59285f
Longle290204 <longvis2004@gmail.com>
2025-06-07 17:23:35 +0700

    feat(auth): implement @Public decorator for public routes

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -1,32 +1,37 @@
 import { JwtService } from '@nestjs/jwt';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { jwtConstants } from '../constants';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-   constructor(private jwtService: JwtService) {}
+   constructor(
+      private jwtService: JwtService,
+      private reflector: Reflector,
+   ) {}
 
    async canActivate(context: ExecutionContext): Promise<boolean> {
+      
+      const isPublic: boolean = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+         context.getHandler(),
+         context.getClass(),
+      ]);
+      if (isPublic) {
+         return true;
+      }
+
       const request = context.switchToHttp().getRequest();
-
       const token = this.extractTokenFromHeader(request);
-
       if (!token) {
          throw new UnauthorizedException();
       }
-
-      console.log('token', token);
-      console.log('secret', jwtConstants.secret);
-      
-
       try {
          console.log('chạy vào đây 1');
-
          const payload = await this.jwtService.verifyAsync(token, {
             secret: jwtConstants.secret,
          });
          console.log('payload:', payload);
-
          request['user'] = payload;
       } catch (error) {
          console.error('⚠️ JWT Verify Error:', error.name, error.message);
@@ -37,14 +42,13 @@ export class AuthGuard implements CanActivate {
    }
 
    private extractTokenFromHeader(request: Request): string | undefined {
-      const authorizationHeader = request.headers['authorization'];
 
+      const authorizationHeader = request.headers['authorization'];
       if (!authorizationHeader) {
          return undefined;
       }
 
       const [type, token] = authorizationHeader.split(' ');
-
       if (type != 'Bearer' && !token) {
          return undefined;
       }
